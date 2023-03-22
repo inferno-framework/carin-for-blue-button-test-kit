e556e52dbc2331f733df71dd573227e5ecb147dc
Lucas Tucker <ltucker@leaporbit.com>
2023-03-22 10:18:52 -0400

    Comment out TLS Version Test

--- lib/carin4bb/capability_statement/capability_statement_group.rb
+++ lib/carin4bb/capability_statement/capability_statement_group.rb
@@ -30,16 +30,16 @@ module CARINForBlueButton
         'RelatedPerson' => ['http://hl7.org/fhir/us/carin-bb/StructureDefinition/C4BB-RelatedPerson'].freeze
     }.freeze
 
-      test from: :tls_version_test,
-          id: :standalone_auth_tls,
-          title: 'FHIR server secured by transport layer security',
-          description: %(
-            Systems **SHALL** use TLS version 1.2 or higher for all transmissions
-            not taking place over a secure network connection.
-          ),
-          config: {
-            options: {  minimum_allowed_version: OpenSSL::SSL::TLS1_2_VERSION } #TODO: Confirm that  we require this version or higher
-          }
+#    test from: :tls_version_test,
+#        id: :standalone_auth_tls,
+#        title: 'FHIR server secured by transport layer security',
+#        description: %(
+#          Systems **SHALL** use TLS version 1.2 or higher for all transmissions
+#          not taking place over a secure network connection.
+#        ),
+#        config: {
+#          options: {  minimum_allowed_version: OpenSSL::SSL::TLS1_2_VERSION } #TODO: Confirm that  we require this version or higher
+#        }   
 
       test from: :carin_bb_conformance_support
       test from: :carin_bb_fhir_version
