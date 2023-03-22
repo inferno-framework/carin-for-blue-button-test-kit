13215780c59237adc8a3867f26ca25b3a81ff8b0
Lucas Tucker <ltucker@leaporbit.com>
2023-03-22 10:57:34 -0400

    JSON Support Test conforms to C4BB documentation

--- lib/carin4bb/capability_statement/tests/json_support_test.rb
+++ lib/carin4bb/capability_statement/tests/json_support_test.rb
@@ -5,12 +5,11 @@ module CARINForBlueButton
     description %(
       FHIR provides multiple [representation
       formats](https://www.hl7.org/fhir/formats.html) for resources, including
-      JSON and XML. US Core profiles require servers to use the [JSON
+      JSON and XML. CARIN BlueButton profiles require servers to use the [JSON
       representation](https://www.hl7.org/fhir/json.html):
 
-      [```The US Core Server **SHALL** Support json source formats for all US
-      Core
-      interactions.```](http://hl7.org/fhir/us/core/CapabilityStatement-us-core-server.html#behavior)
+      [```The C4BB Server **SHALL** Support json source formats for all CARIN-BB
+      interactions.```](http://hl7.org/fhir/us/carin-bb/STU2/CapabilityStatement-c4bb.html#behavior)
 
       The FHIR conformance interaction require servers to describe which
       formats are available for clients to use. The server must explicitly
