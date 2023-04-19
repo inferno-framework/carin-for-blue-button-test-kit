4118c2335362d76c2c8a9ce4b5f5b17690ff5f3d
Stephen MacVicar <smacvicar@mitre.org>
2023-04-19 09:21:01 -0400

    fix paths

--- lib/carin_for_blue_button_test_kit.rb
+++ lib/carin_for_blue_button_test_kit.rb
@@ -1,2 +1,2 @@
-require 'carin4bb/generated/v1.1.0/c4bb_test_suite'
-require 'carin4bb/generated/v2.0.0/c4bb_test_suite'
+require_relative 'carin_for_blue_button_test_kit/generated/v1.1.0/c4bb_test_suite'
+require_relative 'carin_for_blue_button_test_kit/generated/v2.0.0/c4bb_test_suite'
