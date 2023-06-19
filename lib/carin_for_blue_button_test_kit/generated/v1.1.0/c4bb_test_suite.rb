600fa59dbdb63990d04aa2d62e482abc12977f52
Karl Naden <knaden@mitre.org>
2023-06-19 16:21:17 -0400

    FI-1954: add test suite description with FHIR

--- lib/carin_for_blue_button_test_kit/generated/v1.1.0/c4bb_test_suite.rb
+++ lib/carin_for_blue_button_test_kit/generated/v1.1.0/c4bb_test_suite.rb
@@ -18,6 +18,9 @@ module CarinForBlueButtonTestKit
   module CARIN4BBV110
     class C4BBTestKit < Inferno::TestSuite
       title 'CARIN IG for Blue Button® v1.1.0'
+      description %(
+        The CARIN for Blue Button test suite validates system conformance to the HL7® FHIR® [CARIN for Blue Button® Implementation Guide](http://hl7.org/fhir/us/carin-bb/STU1.1).
+      )
       version VERSION
       links [
         {
