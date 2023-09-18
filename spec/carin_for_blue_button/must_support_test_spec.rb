db842e93d0e0ad0d359b78c04f11c3f0ae59212b
Alisa Wallace <awallace@mitre.org>
2023-09-18 14:44:11 -0700

    Read, print, and parse sample Patient resource

--- spec/carin_for_blue_button/must_support_test_spec.rb
+++ spec/carin_for_blue_button/must_support_test_spec.rb
@@ -1,3 +1,16 @@
 RSpec.describe CarinForBlueButtonTestKit::MustSupportTest do
     puts "Must support test!"
+    let(:json_string) do 
+        File.read(File.join(__dir__, '..', 'fixtures', 'c4bb_patient_example.json'))
+    end 
+    
+    let(:patient_resource) { FHIR.from_contents(json_string) }
+    
+    it 'prints out the contents' do
+        puts json_string
+    end
+
+    it 'creates a resource' do 
+        puts patient_resource.id
+    end
 end
