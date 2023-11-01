d96c97907fd15c05e3850dc2b4192fc3644a56f5
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2023-11-01 09:37:22 -0400

    Fix typo in optional? method in search test generator

--- lib/carin_for_blue_button_test_kit/generator/search_test_generator.rb
+++ lib/carin_for_blue_button_test_kit/generator/search_test_generator.rb
@@ -137,7 +137,7 @@ module CarinForBlueButtonTestKit
       end
 
       def optional?
-        conformance_expectation != 'SHALL' || search_metadata[:must_support_or_mandatory]
+        conformance_expectation != 'SHALL' || !search_metadata[:must_support_or_mandatory]
       end
 
       def search_definition(name)
