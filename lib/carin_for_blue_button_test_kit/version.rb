1b96850797958ffc3128a597e00b2da0a19bae30
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-02-25 14:43:38 -0500

    Release 0.14.0 (#66)
    * Bump version to 0.14.0
    
    * update changelog
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    ---------
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    Co-authored-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/carin_for_blue_button_test_kit/version.rb
+++ lib/carin_for_blue_button_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module CarinForBlueButtonTestKit
-  VERSION = '0.13.3'
-  LAST_UPDATED = '2025-02-04'.freeze # TODO: update next release
+  VERSION = '0.14.0'
+  LAST_UPDATED = '2025-02-25'.freeze
 end
