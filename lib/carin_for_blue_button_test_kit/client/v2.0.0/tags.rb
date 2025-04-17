12f2b7dc48843ce1b1645fb69c95313fab779267
Karl Naden <knaden@mitre.org>
2025-04-17 11:37:35 -0400

    harmonize tags, make suite overridable

--- lib/carin_for_blue_button_test_kit/client/v2.0.0/tags.rb
+++ lib/carin_for_blue_button_test_kit/client/v2.0.0/tags.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module CarinForBlueButtonTestKit
-  RESOURCE_API_TAG = 'carin_resource_api'
-  RESOURCE_ID_TAG = 'carin_resource_id'
+  RESOURCE_API_TAG = 'resource_api'
+  RESOURCE_ID_TAG = 'resource_id'
   DEDICATED_ENDPOINTS_AUTH_TAG = 'dedicated_endpoints_auth'
 end
