8b282795cd6c0c5ecba7fcbfba035c34994115f0
Karl Naden <knaden@mitre.org>
2025-04-16 20:15:50 -0400

    bump smart and udap

--- lib/carin_for_blue_button_test_kit/client/v2.0.0/client_auth/token_udap_verification_test.rb
+++ lib/carin_for_blue_button_test_kit/client/v2.0.0/client_auth/token_udap_verification_test.rb
@@ -1,7 +1,7 @@
 require 'udap_security_test_kit'
 
 module CarinForBlueButtonTestKit
-  class C4BBV200ClientTokenUDAPVerification < UDAPSecurityTestKit::UDAPClientTokenRequestVerification
+  class C4BBV200ClientTokenUDAPVerification < UDAPSecurityTestKit::UDAPClientTokenRequestAuthorizationCodeVerification
     id :c4bb_v200_client_token_udap_verification
 
     def client_suite_id
--- lib/carin_for_blue_button_test_kit/client/v2.0.0/client_registration/udap_verification_test.rb
+++ lib/carin_for_blue_button_test_kit/client/v2.0.0/client_registration/udap_verification_test.rb
@@ -1,7 +1,7 @@
 require 'udap_security_test_kit'
 
 module CarinForBlueButtonTestKit
-  class C4BBV200ClientRegistrationUDAPVerification < UDAPSecurityTestKit::UDAPClientRegistrationVerification
+  class C4BBV200ClientRegistrationUDAPVerification < UDAPSecurityTestKit::UDAPClientRegistrationAuthorizationCodeVerification
     id :c4bb_v200_client_reg_udap_verification
 
     def client_suite_id
