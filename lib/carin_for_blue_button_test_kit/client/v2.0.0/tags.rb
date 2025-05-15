6434ced0dfd0e173d5c83211908ff4d47cb2ff16
Karl Naden <knaden@mitre.org>
2025-05-15 09:45:35 -0400

    harmonize carin data access tags with pdex

--- lib/carin_for_blue_button_test_kit/client/v2.0.0/tags.rb
+++ lib/carin_for_blue_button_test_kit/client/v2.0.0/tags.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module CarinForBlueButtonTestKit
-  RESOURCE_API_TAG = 'carin_resource_api'
-  RESOURCE_ID_TAG = 'carin_resource_id'
+  RESOURCE_API_TAG = 'resource_api'
+  RESOURCE_ID_TAG = 'resource_id'
 end
