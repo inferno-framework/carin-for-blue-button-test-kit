f5a644cc79fd3f300e1c456ec4cce75158b2f5b1
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-06-23 18:00:36 -0400

    fix eob/insurer_same_test

--- lib/carin_for_blue_button_test_kit/custom_groups/v2.0.0/eob/insurer_same_test.rb
+++ lib/carin_for_blue_button_test_kit/custom_groups/v2.0.0/eob/insurer_same_test.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-require 'pry'
-require 'pry-byebug'
 
 module CarinForBlueButtonTestKit
   module CARIN4BBV200
@@ -13,13 +11,13 @@ module CarinForBlueButtonTestKit
       in the ExplanationOfBenefit.insurance list with focal=True, and that ExplanationOfBenefit.insurer is not 
       equal to any insurance referenced in the ExplanationOfBenefit.insurance list with focal=False.
       )
-      
+
       verifies_requirements 'hl7.fhir.us.carin-bb_2.0.0@116',
-        'hl7.fhir.us.carin-bb_2.0.0@124',
-        'hl7.fhir.us.carin-bb_2.0.0@125',
-        'hl7.fhir.us.carin-bb_2.0.0@127',
-        'hl7.fhir.us.carin-bb_2.0.0@128',
-        'hl7.fhir.us.carin-bb_2.0.0@129'
+                            'hl7.fhir.us.carin-bb_2.0.0@124',
+                            'hl7.fhir.us.carin-bb_2.0.0@125',
+                            'hl7.fhir.us.carin-bb_2.0.0@127',
+                            'hl7.fhir.us.carin-bb_2.0.0@128',
+                            'hl7.fhir.us.carin-bb_2.0.0@129'
 
       def resource_type
         'ExplanationOfBenefit'
@@ -39,17 +37,26 @@ module CarinForBlueButtonTestKit
         resources = scratch_resources[:all]
         skip_if resources.blank?,
                 "No #{resource_type} resources were returned"
-  
+
         resources.each do |eob|
-          eob.insurance.each do |insurance|
+          eob.insurance.each_with_index do |insurance, index|
             coverage = get_reference(insurance.coverage, :coverage)
+
+            skip_if coverage.nil?,
+                    "No insurance coverage found for EOB #{eob.id} insurance at index #{index}"
 
             if insurance.focal
               assert resource_id(eob.insurer) == resource_id(coverage.payor.first),
-               "ExplanationOfBenefit.insurer differs from ExplanationOfBenefit.insurance.coverage.payor when ExplanationOfBenefit.insurance.focal == True. They must be equal."
+                     <<~ERROR_MESSAGE1
+                        ExplanationOfBenefit.insurer differs from ExplanationOfBenefit.insurance.coverage.payor
+                        when ExplanationOfBenefit.insurance.focal == True. They must be equal.
+                      ERROR_MESSAGE1
             else
               assert resource_id(eob.insurer) != resource_id(coverage.payor.first),
-               "ExplanationOfBenefit.insurer equals ExplanationOfBenefit.insurance.coverage.payor when ExplanationOfBenefit.insurance.focal == False. They must differ."
+                     <<~ERROR_MESSAGE2
+                        ExplanationOfBenefit.insurer equals ExplanationOfBenefit.insurance.coverage.payor
+                        when ExplanationOfBenefit.insurance.focal == False. They must differ.
+                      ERROR_MESSAGE2
             end
           end
         end
