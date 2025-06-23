e6497c5aa8ff81e522cf92777967ccf6570edb7e
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-06-23 17:44:36 -0400

    address feedback on type data absent test

--- lib/carin_for_blue_button_test_kit/custom_groups/v2.0.0/eob/type_data_absent_test.rb
+++ lib/carin_for_blue_button_test_kit/custom_groups/v2.0.0/eob/type_data_absent_test.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module CarinForBlueButtonTestKit
   module CARIN4BBV200
     class TypeDataAbsentTest < Inferno::Test
@@ -7,10 +8,10 @@ module CarinForBlueButtonTestKit
       description %(
         ExplanationOfBenefit.type SHALL NOT use a data absent reason.
       )
-      
+
       verifies_requirements 'hl7.fhir.us.carin-bb_2.0.0@115'
 
-      DAR_CODE_SYSTEM_URL = 'http://terminology.hl7.org/CodeSystem/data-absent-reason'.freeze
+      DAR_CODE_SYSTEM_URL = 'http://terminology.hl7.org/CodeSystem/data-absent-reason'
 
       def resource_type
         'ExplanationOfBenefit'
@@ -22,14 +23,16 @@ module CarinForBlueButtonTestKit
 
       run do
         resources = scratch_resources[:all]
-  
+
         skip_if resources.blank?,
                 "No #{resource_type} resources were returned"
-  
+
         resources.each do |resource|
-          eob_type = resource.type
-          eob_type.coding.each do |coding|
-            assert coding.system != DAR_CODE_SYSTEM_URL, 'ExplanationOfBenefit.type uses a data absent reason. It must not.'
+          resource.type&.coding&.each do |coding|
+            if coding.system
+              assert coding.system != DAR_CODE_SYSTEM_URL,
+                     'ExplanationOfBenefit.type uses a data absent reason. It must not.'
+            end
           end
         end
       end
