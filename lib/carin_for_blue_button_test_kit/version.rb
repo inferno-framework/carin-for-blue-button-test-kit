879e5825897853fe9c730ec4d7679dd6c4d82164
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-07-02 15:51:04 -0400

    bump version, gemfile lock

--- lib/carin_for_blue_button_test_kit/version.rb
+++ lib/carin_for_blue_button_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module CarinForBlueButtonTestKit
-  VERSION = '0.15.2'
-  LAST_UPDATED = '2025-07-02'.freeze
+  VERSION = '0.15.3'
+  LAST_UPDATED = '2025-07-02'
 end
