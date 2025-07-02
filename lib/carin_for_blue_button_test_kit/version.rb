a43b6b57e28abb6c27b9fc26bc63b8bd14eb5dab
Karl Naden <86612928+karlnaden@users.noreply.github.com>
2025-07-02 13:55:39 -0400

    Release 0.15.2 (#83)
    * bump version
    
    * update changelog

--- lib/carin_for_blue_button_test_kit/version.rb
+++ lib/carin_for_blue_button_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module CarinForBlueButtonTestKit
-  VERSION = '0.15.1'
-  LAST_UPDATED = '2025-05-20'.freeze
+  VERSION = '0.15.2'
+  LAST_UPDATED = '2025-07-02'.freeze
 end
